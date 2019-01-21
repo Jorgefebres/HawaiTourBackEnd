2ff96d75df95b27b0d9e6c9a0526378dbd69187d
Jorge Febres <44093878+Jorgefebres@users.noreply.github.com>
2019-01-21 11:44:38 -0500

    segundo commit

--- controllers/categoriaCtrl.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-var categoriaController = {
-    home: function (req, res){
-        return res.status(200).send({message : "Home del controlador categoría!!!"});
-    },
-}
-module.exports = categoriaController;
--- controllers/productoCtrl.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-var productoController = {
-    home: function (req, res){
-        return res.status(200).send({message : "Home del controlador producto!!!"});
-    },
-}
-module.exports = productoController;
